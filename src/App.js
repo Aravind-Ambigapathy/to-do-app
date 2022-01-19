498a01eecc5f884c8351543a05f97591eb07fb9f
aravind <aravindnsit2015@gmail.com>
2022-01-19 16:59:13 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import TodoApp from './components/TodoApp';
+import TodoApp from './components1/TodoApp';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TodoApp/> */}
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
